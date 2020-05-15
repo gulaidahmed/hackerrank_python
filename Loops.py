9cfc9f5fbedfbf66f2ebcc4f7777552f63a9e870
Gulaid Ahmed <52134901+gulaidahmed@users.noreply.github.com>
2020-05-15 23:05:39 +0100

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input())
+    while n < 5:
+        print(i**i)
---
+++ PrintFunction.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        j = n*i/n
+        print(int(j), end='')
